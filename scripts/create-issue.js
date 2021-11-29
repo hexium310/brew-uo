78f0807aa761f18af2ab1c5ce66df7e54a2e1692
Hexin <hexium310@gmail.com>
2021-11-30 02:58:04 +0900

    Check unsupported version delimiter daily

---
+++ scripts/create-issue.js
@@ -0,0 +1,69 @@
+const marked = require('marked');
+const { diff } = require('deep-object-diff');
+
+const buildBody = (content) => {
+  return [
+    '```json',
+    content,
+    '```'
+  ].join('\n');
+};
+
+const createIssue = (content) => {
+  const issue = await github.rest.issues.create({
+    owner,
+    repo,
+    title: 'Unsupported version delimiters',
+    body: buildBody(content),
+    label: 'unsupported-version',
+  });
+
+  console.log(`Create ${ issue.html_url }`);
+};
+
+const comment = (issue, content) => {
+  const issueNumber = issue.number;
+  const comments = await github.rest.issues.listComments({
+    owner,
+    repo,
+    issue_number: issueNumber,
+  });
+  const bodies = [issue.body, comments.map(({ body }) => body)];
+  const mentionedVersions = bodies.map((body) => {
+    const tokens = marked.lexer(body);
+    const codes = tokens.filter(({ type, lang }) => type === 'code' && lang === 'json');
+    return codes.map(({ text }) => JSON.parse(text));
+  }).flat();
+  const versions = JSON.parse(content);
+  const diffKeys = Object.keys(diff(mentionedVersions, versions));
+  const diffVersions = versions.filter((obj) => diffKeys.includes(Object.keys(obj)[0]));
+  const comment = await github.rest.issues.createComment({
+    owner,
+    repo,
+    issue_number: issueNumber,
+    body: buildBody(JSON.stringify(diffVersions, null, 2)),
+  });
+
+  console.log(`Comment ${ comment.html_url }`);
+};
+
+module.exports = async ({ github, context, core }) => {
+  const { VERSIONS } = process.env;
+  const { owner, repo } = context.repo;
+
+  const issues = await github.rest.issues.listForRepo({
+    owner,
+    repo,
+    state: 'open',
+    label: 'unsupported-version',
+  });
+
+  if (issues.length === 0) {
+    createIssue(VERSIONS);
+    return;
+  }
+
+
+  const latestIssue = issues[issues.lenght - 1];
+  comment(latestIssue, VERSIONS);
+}
